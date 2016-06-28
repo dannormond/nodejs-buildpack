1a94d6c47ddf519be97fc08526b891874459044c
Dann Ormond <dann.ormond@gmail.com>
2016-06-28 08:05:44 -0600

    Removed rebuild of native components

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -20,8 +20,8 @@ rebuild_node_modules() {
 
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
-    echo "Rebuilding any native modules"
-    npm rebuild --nodedir=$build_dir/.heroku/node 2>&1
+    echo "Skipping rebuild of native modules"
+    #npm rebuild --nodedir=$build_dir/.heroku/node 2>&1
     if [ -e $build_dir/npm-shrinkwrap.json ]; then
       echo "Installing any new modules (package.json + shrinkwrap)"
     else
